f6caf676c938bf14d6e75ab00b534f1a52657d89
ZMaks022 <zmaks022@gmail.com>
2019-08-23 18:42:25 +0300

    Fix slow load

--- NewProcess/WriteResponse/WriteResponse.cpp
+++ NewProcess/WriteResponse/WriteResponse.cpp
@@ -14,22 +14,12 @@ void writeResponse (int readPipe, int fd_client) {
    read(readPipe, response, buffSize);
 
    if (response[buffSize-1] == '\f') {
-      while (r - l > 1) {
-         l = 0;
-         r = buffSize - 1;
-         if (response[(l + r) / 2] == '\f') {
-            r = (l + r) / 2;
-         } else {
-            l = (l + r) /2;
+      for (int i = buffSize-1; i >= 0; i++) {
+         if (response[i] != '\f') {
+            write(fd_client, response, i);
+            return;
          }
       }
-
-      if (response[l] == '\f') {
-         write(fd_client, response, l-1);
-      } else {
-         write(fd_client, response, r-1);
-      }
-      return;
    }
 
    // if not found \f
