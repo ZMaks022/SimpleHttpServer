6dbf389c108438f8554c7a16314a0431cacd403c
ZMaks022 <zmaks022@gmail.com>
2019-08-24 12:23:18 +0300

    404 try to fix

--- main.cpp
+++ main.cpp
@@ -71,10 +71,12 @@ int main(int argc, char* argv[]) {
          char* url = ParseUrl(buf, bufSize);
          if (CheckStaticFile(url)) {
             if (!CheckAvailabilitySymbols(url)) {
+               std::cout << 1 << std::endl;
                close(fd_client);
                exit(0); // rewrite on sending header of error
             }
             if (!AvailableExt(url)) {
+               std::cout << 2 << std::endl;
                close(fd_client);
                exit(0); // rewrite on sending header of error
             }
