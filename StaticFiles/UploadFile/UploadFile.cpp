deec4a5ed8ca4238ca9cb1afe7053428141464cd
Max <zmaks022@gmail.com>
2019-10-15 18:56:52 +0300

    Dev

--- StaticFiles/UploadFile/UploadFile.cpp
+++ StaticFiles/UploadFile/UploadFile.cpp
@@ -2,9 +2,11 @@
 // Created by user on 2019-08-23.
 //
 #include "UploadFile.h"
+#include <iostream>
+using namespace std;
 
 void UploadFile (const char* path, int fd_client) noexcept {
-   const char* home = "DefaultFrontEnd"; // frontend directory
+   const char* home = "/home/zakhar/frontend"; // frontend directory
    struct stat* buf;
    char fullpath[strlen(home)+strlen(path)+1];
    strcpy(fullpath, home);
--- StaticFiles/UploadFile/UploadFile.h
+++ StaticFiles/UploadFile/UploadFile.h
@@ -5,21 +5,12 @@
 #ifndef WEBSERVER_UPLOADFILE_H
 #define WEBSERVER_UPLOADFILE_H
 
-// TODO move define to main.cpp
-#define LINUX
-//#define MACOS
-
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <string.h>
-
-#ifdef LINUX
-   #include <sys/sendfile.h>
-#endif
-
-
+#include <sys/sendfile.h>
 
 void UploadFile (const char* path, int fd_client) noexcept ;
 
